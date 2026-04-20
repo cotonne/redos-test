bf974d38b29ddd31d992c5aeb1f4182305aa1b5b
Cotonne <cotonne@users.noreply.github.com>
2026-04-20 11:37:21 +0000

    fix: minor python issues

--- src/app.py
+++ src/app.py
@@ -3,10 +3,10 @@ import re
 def test_pattern(s: str):
   patterns = [
     re.compile("\\[.*\\]"),
-    re.compile("<\s*img[^>]*>.*?<\s*/\s*img\s*>"),
+    re.compile("<\\s*img[^>]*>.*?<\\s*/\\s*img\\s*>"),
     re.compile("A(B|C+)+D"),
   ]
-  return any([p.match(s) for p in patterns])
+  return any(p.match(s) for p in patterns)
 
 if __name__ == "__main__":
   test_str = "[ok]"
